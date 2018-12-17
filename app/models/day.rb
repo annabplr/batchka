6a75de0bed6c7a4b2b87ceb59fbffd4dcbee8b7c
Annabelle PELLIER <annabplr@gmail.com>
2018-12-17 12:39:06 +0100

    models creation

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,7 @@
 class Day < ApplicationRecord
+  belongs_to :menus
+  has_many :repas
+  has_many :plats, through: :repas
+  has_many :ingredients, through: :plats
+  has_many :todos, through: :ingredients
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,9 @@
 class Menu < ApplicationRecord
+  belongs_to :user
+  has_many :days, dependent: :destroy
+  has_many :todos, dependent: :destroy
+  has_many :ingredients, through: :todos
+  has_many :repas, through: :days
+  has_many :plats, through: :repas
+
 end
