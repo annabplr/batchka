0f7adf44b3619680de33d46fd5939ffa891ee02c
Annabelle PELLIER <annabplr@gmail.com>
2018-12-18 16:28:13 +0100

    create new menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,16 +3,45 @@ class MenusController < ApplicationController
   before_action :set_menu, only: [:show, :update, :destroy]
 
   def new
-    @menu = Menu.create
+    @menu = Menu.new
   end
 
   def create
     @menu = Menu.new(params_menu)
-    if @menu.save
-      redirect_to menu_path(@menu)
+    @current_user = current_user
+    @menu.user_id = @current_user.id
+
+    @day = Menu.last.startdate.split('-')
+    @weekday = Date.new(day[0].to_i, day[1].to_i, day[2].to_i).wday
+
+    if @day[1] == "06" || @day[1] == "07" || @day[1] == "08"
+      @menu.season = "summer"
+    elsif @day[1] == "09" || @day[1] == "10" || @day[1] == "11"
+      @menu.season = "fall"
+    elsif @day[1] == "12" || @day[1] == "01" || @day[1] == "02"
+      @menu.season = "winter"
     else
-      render_new
+      @menu.season = "spring"
     end
+
+    if @weekday == 1
+      @menu.weekof = "monday"
+    elsif @weekday == 2
+      @menu.weekof = "tuesday"
+    elsif @weekday == 3
+      @menu.weekof = "wednesday"
+    elsif @weekday == 4
+      @menu.weekof = "thursday"
+    elsif @weekday == 5
+      @menu.weekof = "friday"
+    elsif @weekday == 6
+      @menu.weekof = "saturday"
+    elsif @weekday == 0
+      @menu.weekof = "sunday"
+    end
+
+    @menu.save!
+    redirect_to menu_path(@menu)
   end
 
   def edit
@@ -30,7 +59,7 @@ class MenusController < ApplicationController
   end
 
   def show
-    @menu
+
   end
 
   private
@@ -40,6 +69,6 @@ class MenusController < ApplicationController
   end
 
   def params_menu
-    params.require(:menu).permit(:weekof, :season)
+    params.require(:menu).permit(:startdate)
   end
 end
