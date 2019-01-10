4f921159bc4007ea984cf276a27cfb12d43e9cae
Annabelle PELLIER <annabplr@gmail.com>
2019-01-10 13:26:16 +0100

    css day show + responsive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,4 @@ import "bootstrap";
 import "../components/edit_ingredient.js";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+import "../components/checkbox-boolean-animation.js";
