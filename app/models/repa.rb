52ca632b4265a34dd83f02fad37d509269df8370
Annab <annabplr@gmail.com>
2019-03-21 11:27:37 +0100

    add indicator if plat is completed

--- app/models/repa.rb
+++ app/models/repa.rb
@@ -3,4 +3,8 @@ class Repa < ApplicationRecord
   has_many :plats
   has_many :ingredients, through: :plats
   has_many :todos, through: :ingredients
+
+  def uncompleted?
+    plats.where.not(name: nil).none?
+  end
 end
